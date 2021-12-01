5cc6294fb137f39d6d43a22b682479f0638799fa
michaelfebrianto <michaelfebrianto@gmail.com>
2021-12-02 00:31:27 +1100

    20211201 - fix bug

--- src/libs/calculate.ts
+++ src/libs/calculate.ts
@@ -4,7 +4,7 @@ export const score = (answers, questions): number => {
   var score = 0
 
   answers.forEach(answer => {
-    var question = questions.find(question => question.id = answer.question_id)
+    var question = questions.find(question => question.id == answer.question_id)
     if (answer.answer == question.answer ) {
       score += 100
       score = score - answer.answer_time
